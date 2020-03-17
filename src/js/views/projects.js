54739a9fd52a46b7c79e705e22b6e3a67011a1ad
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-03-17 16:39:50 +0000

    projects progress

--- src/js/views/projects.js
+++ src/js/views/projects.js
@@ -9,9 +9,17 @@ export const Projects = () => {
 			<div className="projects-container-child d-flex                 justify-content-center">
 				<h1 className="projects-h1">Open Source Projects</h1>
 			</div>
-			<br />
 			<div className="projects-cards-top d-flex justify-content-around">
 				<Card />
+			</div>
+			<br />
+			<div className="more-projects d-flex                 justify-content-center">
+				<button
+					className="more-projects-button btn btn-secondary btn-lg"
+					target="_blank"
+					rel="noopener noreferrer">
+					More Projects
+				</button>
 			</div>
 		</div>
 	);
