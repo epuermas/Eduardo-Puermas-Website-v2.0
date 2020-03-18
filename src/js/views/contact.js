7047cd89bd01d277aa8d98fae331015927f0d9d5
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-03-18 15:48:01 +0000

    contact me section cosmetic

--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -6,20 +6,25 @@ import { Context } from "../store/appContext";
 export const Contact = () => {
 	return (
 		<div className="contact-container">
-			<div className="skills-container d-flex justify-content-center">
-				{/* Left side img */}
-				<div className="skills-lrcontainer">
-					<div className="skills-limg">
-						<img className="skills-img" src="https://i.imgur.com/IzHZHkK.gif" />
-					</div>
-				</div>
+			<div className="contact-container d-flex justify-content-center">
 				{/* Right side text */}
-				<div className="skills-rtext">
-					<h1 className="skills-h1">What I do</h1>
-					<h4 className="skills-h2">
-						These are the languages I have learned so far, the list is growing and constantly being updated:
+				<div className="contact-rtext">
+					<h1 className="contact-h1">Contact Me ☎️</h1>
+					<h4 className="contact-h2">
+						To discuss a project or just want to say hi, my inbox is open for all.
 						<br />
 					</h4>
+					<h2 className="contact-info">
+						+1-786-317-8698
+						<br />
+						eduardopuermas@hotmail.com
+					</h2>
+				</div>
+				{/* Left side img */}
+				<div className="contact-lrcontainer">
+					<div className="contact-limg">
+						<img className="contact-img" src="https://i.imgur.com/NJQT0Ue.gif" />
+					</div>
 				</div>
 			</div>
 		</div>
