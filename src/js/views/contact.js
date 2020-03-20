c80e52a4861596ca6d45d97f26cceba0fd4f9e54
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-03-20 14:50:06 +0000

    minor update

--- src/js/views/contact.js
+++ src/js/views/contact.js
@@ -12,42 +12,50 @@ export const Contact = () => {
 					<h1 className="contact-h1">Contact Me ☎️</h1>
 					<h4 className="contact-h2">
 						To discuss a project or just want to say hi, my inbox is open for all:
-						<br />
 					</h4>
-					<h2 className="contact-info">
-						786-317-8698
-						<br />
-						eduardopuermas@hotmail.com
-					</h2>
-					<div className="contact-lsocial">
-						<a
-							className="fab fa-github-square"
-							href="https://github.com/epuermas"
-							id="github"
-							target="_blank"
-							rel="noopener noreferrer"
-						/>
-						<a
-							className="fab fa-linkedin"
-							href="https://www.linkedin.com/in/epuermas/"
-							id="linkedin"
-							target="_blank"
-							rel="noopener noreferrer"
-						/>
-						<a
-							className="fab fa-facebook-square"
-							href="https://www.facebook.com/HeartagramEd"
-							id="facebook"
-							target="_blank"
-							rel="noopener noreferrer"
-						/>
-						<a
-							className="fab fa-twitter-square"
-							href="https://twitter.com/Eddy_Prms"
-							id="twitter"
-							target="_blank"
-							rel="noopener noreferrer"
-						/>
+					<div className="photo-and-text d-flex justify-content-start">
+						<div className="photo">
+							<img className="contact-photo" src="https://i.imgur.com/lwg5om9.jpg" />
+						</div>
+						<div className="next-to-photo">
+							<h2 className="contact-info">
+								Eduardo Puermas
+								<br />
+								786-317-8698
+								<br />
+								eduardopuermas@hotmail.com
+							</h2>
+							<div className="contact-lsocial">
+								<a
+									className="fab fa-github-square"
+									href="https://github.com/epuermas"
+									id="github"
+									target="_blank"
+									rel="noopener noreferrer"
+								/>
+								<a
+									className="fab fa-linkedin"
+									href="https://www.linkedin.com/in/epuermas/"
+									id="linkedin"
+									target="_blank"
+									rel="noopener noreferrer"
+								/>
+								<a
+									className="fab fa-facebook-square"
+									href="https://www.facebook.com/HeartagramEd"
+									id="facebook"
+									target="_blank"
+									rel="noopener noreferrer"
+								/>
+								<a
+									className="fab fa-twitter-square"
+									href="https://twitter.com/Eddy_Prms"
+									id="twitter"
+									target="_blank"
+									rel="noopener noreferrer"
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
 				{/* Left side img */}
