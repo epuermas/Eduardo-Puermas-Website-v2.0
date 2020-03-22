b450b1b13eab2941b78568ac069dff7c5a79f334
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-03-22 20:21:27 +0000

    aws ready

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
+//include bootstrap npm library into the bundle.
 import "bootstrap/dist/css/bootstrap.css";
 
-//include your index.scss file into the bundle
+//include your index.scss file into the bundle.
 import "../styles/index.scss";
 
 //import your own components
