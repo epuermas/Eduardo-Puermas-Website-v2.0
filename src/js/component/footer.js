9e0af08556e60472ac4b230bf29f48d20f158e6a
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-06-06 15:57:06 +0000

    minor update

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<img className="footer-logo" src="https://i.imgur.com/KK3xtxv.png" />
+		<img className="footer-logo" src="https://i.imgur.com/uZlJ0ZH.png" />
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="https://github.com/epuermas" target="_blank" rel="noopener noreferrer">
